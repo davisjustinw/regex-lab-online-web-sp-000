a9cec3f8665a464bbccf899ae5448117220b9f81
Justin Davis <davisjustinw@gmail.com>
2019-01-08 00:08:12 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def starts_with_a_vowel?(word)
-  word.match(/\A[aeiou]/) ? true : false
+  word.match(/\A[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
